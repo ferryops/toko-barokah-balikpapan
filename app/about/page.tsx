7d6e826c50af30990ff296a3e746ea6d431220fc
ferryops <ferry.a.febian@gmail.com>
2023-08-26 00:19:41 +0800

    initial project

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,15 @@
 import { title } from "@/components/primitives";
 
 export default function AboutPage() {
-	return (
-		<div>
-			<h1 className={title()}>About</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className={title({ size: "md" })}>Tentang Toko Barokah</h1>
+      <h2 className="mt-2">
+        Toko Barokah merupakan toko sembako yang menjual berbagai macam jenis kebutuhan tumah tangga. Awalnya hanya berfokus pada
+        menjual beras, gula, dan minyak, namun seiring waktu, toko ini berkembang menjadi pusat kebutuhan sehari-hari bagi warga
+        sekitar. Melalui pelayanan yang baik dan harga yang terjangkau, toko barokah ini berhasil bertahan dan terus berkembang
+        hingga hari ini dalam memenuhi kebutuhan masyarakat sekitar.
+      </h2>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,37 @@
 import NextLink from "next/link";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
+import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import MainItem from "@/components/main-item";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  return (
+    <>
+      <section className="flex flex-col items-center h-screen gap-4 py-8 md:py-10">
+        <div className=" max-w-lg text-center justify-center">
+          <h1 className={title()}>Temukan&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>kebutuhan&nbsp;</h1>
+          <br />
+          <h1 className={title()}>sembako terlengkap dan harga terbaik di toko barokah Balikpapan</h1>
+          <h2 className={subtitle({ class: "mt-4" })}>Hemat dan Praktis! Temukan Semua Kebutuhan Sembako Anda di Toko Kami.</h2>
+        </div>
+      </section>
+      <section className="flex items-center justify-center">
+        <MainItem />
+      </section>
+      <section className="flex justify-center h-screen items-end">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d547.682837447115!2d116.82429546196119!3d-1.2203250403432375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df147df14acaf77%3A0xffae1c109e093a2c!2sToko%20Barokah!5e0!3m2!1sid!2sid!4v1692978637530!5m2!1sid!2sid"
+          width="600"
+          height="450"
+          className="rounded-md"
+          loading="lazy"
+        ></iframe>
+      </section>
+    </>
+  );
 }
